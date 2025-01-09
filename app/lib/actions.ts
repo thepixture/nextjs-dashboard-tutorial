e6873e6b4482357bb4eafc94368ca62f4bba2088
thepixture <kontakt@thepixture.de>
2025-01-09 13:34:23 +0100

    Chapter 12

---
+++ app/lib/actions.ts
@@ -0,0 +1,60 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    // console.log('Creating invoice:', { customerId, amount, status, date });
+
+    await sql.query(
+        `INSERT INTO invoices (customer_id, amount, status, date) VALUES ($1, $2, $3, $4)`,
+        [customerId, amountInCents, status, date]
+    );
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql.query(
+        `UPDATE invoices SET customer_id = $1, amount = $2, status = $3 WHERE id = $4`,
+        [customerId, amountInCents, status, id]
+    );
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql.query(`DELETE FROM invoices WHERE id = $1`, [id]);
+    revalidatePath('/dashboard/invoices');
+}
